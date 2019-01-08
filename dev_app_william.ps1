0a18553dee1a0755aaeb147614afa232efd5e386
William Denton <william.denton@pushpay.com>
2019-01-09 09:15:58 +1300

    remove script that never existed

--- dev_app_william.ps1
+++ dev_app_william.ps1
@@ -26,7 +26,6 @@ executeScript "SystemConfiguration.ps1";
 executeScript "FileExplorerSettings.ps1";
 executeScript "RemoveDefaultApps.ps1";
 executeScript "CommonDevTools.ps1";
-executeScript "AwsTools.ps1";
 executeScript "WSL.ps1";
 executeScript "Browsers.ps1";
 
