80bc6518c7677771a8e628e10a967983380faa86
William Denton <williamdenton@users.noreply.github.com>
2019-01-09 15:12:40 +1300

    change to provided example as this is erroring
    https://chocolatey.org/packages/git.install

--- scripts/CommonDevTools.ps1
+++ scripts/CommonDevTools.ps1
@@ -1,7 +1,7 @@
 
 # tools we expect devs across many scenarios will want
 choco install -y vscode
-choco install -y git.install --package-parameters='"/GitAndUnixToolsOnPath /WindowsTerminal"'
+choco install -y git.install --params "/GitAndUnixToolsOnPath /WindowsTerminal /SChannel"
 choco install -y python
 choco install -y 7zip.install
 choco install -y sysinternals
