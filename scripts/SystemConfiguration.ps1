75b1acc92466933adc157ce3f831c04a353717cf
William Denton <williamdenton@users.noreply.github.com>
2019-01-09 15:25:43 +1300

    remove policy

--- scripts/SystemConfiguration.ps1
+++ scripts/SystemConfiguration.ps1
@@ -2,6 +2,4 @@
 
 #--- Enable developer mode on the system ---
 Set-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\AppModelUnlock -Name AllowDevelopmentWithoutDevLicense -Value 1
-
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 Install-PackageProvider -Name NuGet -Force
