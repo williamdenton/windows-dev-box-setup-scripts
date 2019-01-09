74ccb631113731543d1315e0ad44bbd19c4a6cd7
William Denton <williamdenton@users.noreply.github.com>
2019-01-09 15:15:46 +1300

    add nuget to powershell

--- scripts/SystemConfiguration.ps1
+++ scripts/SystemConfiguration.ps1
@@ -2,3 +2,6 @@
 
 #--- Enable developer mode on the system ---
 Set-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\AppModelUnlock -Name AllowDevelopmentWithoutDevLicense -Value 1
+
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+Install-PackageProvider -Name NuGet -Force
