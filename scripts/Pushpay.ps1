20f959f145397dac47097fd5cda46487d78cf931
Rowanvdh <rowan.vandenhurk@pushpay.com>
2020-11-23 14:40:17 +1300

    move pushpay specific installs into separate script

---
+++ scripts/Pushpay.ps1
@@ -0,0 +1,44 @@
+#--- Tools ---
+
+Update-SessionEnvironment # refreshing env due to Git install
+
+#--- Installing VS and VS Code with Git
+# See this for install args: https://chocolatey.org/packages/VisualStudio2019Community
+# https://docs.microsoft.com/en-us/visualstudio/install/workload-component-id-vs-community
+# https://docs.microsoft.com/en-us/visualstudio/install/use-command-line-parameters-to-install-visual-studio#list-of-workload-ids-and-component-ids
+
+choco install -y visualstudio2019professional
+choco install -y visualstudio2019-workload-netweb
+choco install -y visualstudio2019-workload-netcoretools
+choco install -y visualstudio2019-workload-netcorebuildtools
+
+choco install -y wsl2
+
+choco install -y dotnetcore-sdk
+choco install -y powershell-core
+choco install -y microsoft-windows-terminal
+choco install -y sql-server-management-studio
+choco install -y awscli
+choco install -y nodejs-lts
+choco install -y yarn
+
+choco install -y vscode-csharp
+choco install -y vscode-docker
+
+choco install -y gitextensions
+choco install -y kdiff3
+choco install -y linqpad
+choco install -y paint.net
+choco install -y fiddler
+
+# for Platform
+choco install -y sql-server-2017
+choco install -y sql2016-clrtypes
+Install-Module -Name SqlServer -Force
+choco install -y NuGet.CommandLine
+choco install -y netfx-4.6.2-devpack
+choco install -y netfx-4.7.2-devpack
+choco install -y rabbitmq --version=3.7.7
+. 'C:\Program Files\RabbitMQ Server\rabbitmq_server-3.7.7\sbin\rabbitmq-plugins' enable rabbitmq_management
+
+choco install -y slack
